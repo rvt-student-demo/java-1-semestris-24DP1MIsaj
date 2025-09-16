1cf5c16e35a2340ef230c4c5eb2c02968702f12e
24DP1MIsaj <A242277MI@rvt.lv>
2025-09-16 14:42:16 +0300

    Average of three numbers

--- src/main/java/rvt/App2.java
+++ src/main/java/rvt/App2.java
@@ -11,8 +11,20 @@ public class App {
         System.out.println("Give the first number: ");
         int num2 = Integer.valueOf(reader.nextLine());
 
-        int result = num1 + num2;
+        int result1 = num1 + num2;
+        int result2 = num1 / num2;
 
-        System.out.println(num1 + " + " + num2 + " = " + result);
+        System.out.println(num1 + " + " + num2 + " = " + result1);
+
+        System.out.println(num1 + " / " + num2 + " = " + result2);
+        // dalot (int) ar (int)
+        // rezultāts vienmēr (int)
+
+        double whenDividendIsFloat = 3.0 / 2;
+        System.out.println(whenDividendIsFloat); // prints 1.5
+
+        double whenDivisorIsFloat = 3 / 2.0;
+        System.out.println(whenDivisorIsFloat); // prints 1.5
     }
+
 }
---
+++ src/main/java/rvt/App3.java
@@ -0,0 +1,23 @@
+package rvt;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Give the first number: ");
+        int num1 = Integer.valueOf(reader.nextLine());
+
+        System.out.println("Give the second number: ");
+        int num2 = Integer.valueOf(reader.nextLine());
+
+        System.out.println("Give the third number: ");
+        int num3 = Integer.valueOf(reader.nextLine());
+
+        int result = num1 + num2 + num3;
+        double result2 = result / 3;
+
+        System.out.println("The average is " + result2);
+    }
+
+}
