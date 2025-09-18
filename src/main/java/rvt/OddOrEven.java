d29808371c99b37e7fed632ca540ed0d4fff402c
24DP1MIsaj <A242277MI@rvt.lv>
2025-09-18 12:41:44 +0300

    Kommit Odd or even

---
+++ src/main/java/rvt/OddOrEven.java
@@ -0,0 +1,19 @@
+package rvt;
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Give a number:");
+        int num = Integer.valueOf(reader.nextLine());
+        reader.close();
+
+        if (num % 2 == 0) {
+            System.out.println("Number " + num + " is even.");
+        } else {
+            System.out.println("Number " + num + " is odd.");
+        }
+    }
+
+}
