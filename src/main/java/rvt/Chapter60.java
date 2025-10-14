4fc47af26eca0b498bccd9fa556cfcb5b4077601
24DP1MIsaj <A242277MI@rvt.lv>
2025-10-14 13:26:47 +0300

    Kommit

--- src/main/java/rvt/Chapter60.java
+++ src/main/java/rvt/Chapter60.java
@@ -3,62 +3,84 @@ package rvt;
 public class Chapter60 {
 
     public static void main(String[] args) {
-        int[] val = {0, 1, 2, 3};
-        ex1();
+        int[] val1 = {0, 1, 2, 3};
+        ex1(val1);
 
-        int[] val = {13, -4, 82, 17};
-        int[] twice = new int[val.length];
-        ex2();
+        int[] val2 = {13, -4, 82, 17};
+        int[] twice = new int[val2.length];
+        ex2(val2, twice);
 
-        int[] valA = { 13, -22,  82,  17}; 
-        int[] valB = {-12,  24, -79, -13};
-        int[] sum = {  0,   0,   0,   0};
-        ex3();
+        int[] valA1 = { 13, -22,  82,  17}; 
+        int[] valB1 = {-12,  24, -79, -13};
+        int[] sum = {0, 0, 0, 0};
+        ex3(valA1, valB1, sum);
 
-        valA = {13, -22, 82, 17}; 
-        valB = {0, 0 ,0 , 0};
-        ex4();
+        int[] valA2 = {13, -22, 82, 17}; 
+        int[] valB2 = {0, 0 , 0, 0};
+        ex4(valA2, valB2);
+
+        int[] val3 = {0, 1, 2, 3}; 
+        int[] temp = new int[val3.length];
+        ex5(val3, temp);
 
     }
 
-    public static void ex1(int[] val) {
+    public static void ex1(int[] val1) {
         int sum = 0;
-        for (i = 0; i < val.length; i++) {
-            sum += val[i];
+        for (int i = 0; i < val1.length; i++) {
+            sum += val1[i];
         }
 
         System.out.println( "Sum of all numbers = " + sum );
     }
 
-    public static void ex2(int[] val, int[] twice) {
-        System.out.println( "Original Array: " + val[0] + " " + val[1] + " " + val[2] + " " + val[3] );
+    public static void ex2(int[] val2, int[] twice) {
+        System.out.println( "Original Array: " + val2[0] + " " + val2[1] 
+        + " " + val2[2] + " " + val2[3] );
 
-        for (i = 0; i < val.length; i++) {
-            twice[i] = val[i] * val[i];
+        for (int i = 0; i < val2.length; i++) {
+            twice[i] = val2[i] * val2[i];
         }
 
-        System.out.println( "New Array: " + twice[0] + " " + twice[1] + " " + twice[2] + " " + twice[3] );
+        System.out.println( "New Array: " + twice[0] + " " + twice[1] 
+        + " " + twice[2] + " " + twice[3] );
     }
 
     public static void ex3(int[] valA, int[] valB, int[] sum) {
-        for (i = 0; i < sum.length; i++) {
+        for (int i = 0; i < sum.length; i++) {
             sum[i] = valA[i] + valB[i];
         }
 
-        System.out.println( "sum: " + sum[0] + " " + sum[1] + " " + sum[2] + " " + sum[3] );
+        System.out.println( "sum: " + sum[0] + " " + sum[1] + " " 
+        + sum[2] + " " + sum[3] );
     }
 
-    public static void ex4(int[] valA, int[] valB) {
-        for (i = 0; i < valB.length; i++) {
-            valB[i] = 25 - valA[i];
+    public static void ex4(int[] valA2, int[] valB2) {
+        for (int i = 0; i < valB2.length; i++) {
+            valB2[i] = 25 - valA2[i];
         }
 
-        System.out.println( "valA: " + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
-        System.out.println( "valB: " + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
-        System.out.println( "sum:  " + (valA[0] + valB[0]) + " " + (valA[1] + valB[1]) + " " + (valA[2] + valB[2]) + " " + (valA[3] + valB[3]) );
+        System.out.println( "valA: " + valA2[0] + " " + valA2[1] 
+        + " " + valA2[2] + " " + valA2[3] );
+
+        System.out.println( "valB: " + valB2[0] + " " + valB2[1] 
+        + " " + valB2[2] + " " + valB2[3] );
+
+        System.out.println( "sum:  " + (valA2[0] + valB2[0]) 
+        + " " + (valA2[1] + valB2[1]) + " " + (valA2[2] + valB2[2]) + " " 
+        + (valA2[3] + valB2[3]) );
     }
 
-    public static void ex5() {
+    public static void ex5(int[] val3, int[] temp) {
+        System.out.println( "Original Array: " 
+        + val3[0] + " " + val3[1] + " " + val3[2] + " " + val3[3] );
 
+        int index = val3.length;
+        for (int i = 0 ; i < val3.length; i++) {
+            temp[i] = val3[index];
+        }
+
+        System.out.println( "Reversed Array: " 
+        + val3[0] + " " + val3[1] + " " + val3[2] + " " + val3[3] );
     }
 }
