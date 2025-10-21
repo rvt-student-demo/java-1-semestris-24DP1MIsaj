d7bd564b8c30e8fb95126d2f242fd17a5b9c233f
24DP1MIsaj <A242277MI@rvt.lv>
2025-10-21 13:22:28 +0300

    Commit: JMArrayList

---
+++ src/main/java/rvt/JMArrayList.java
@@ -0,0 +1,101 @@
+package rvt;
+import java.util.*;
+
+public class JMArrayList {
+    
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+
+        strings.add("First");
+        strings.add("Second");
+        strings.add("Third");
+
+        System.out.println(strings);
+
+        removeLast(strings);
+        removeLast(strings);
+
+        System.out.println(strings);
+    }
+
+    public static void onlyTheseNumbers() {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<Integer> data1 = new ArrayList<>();
+
+        while (true) {
+            int x = Integer.valueOf(reader.nextLine());
+
+            if (x == -1) {
+                break;
+            }
+
+            data1.add(x);
+        }
+
+        System.out.println("From where?");
+        int from = Integer.valueOf(reader.nextLine());
+        System.out.println("To where?");
+        int to = Integer.valueOf(reader.nextLine());
+        reader.close();
+
+        for (int i = from; i <= to; i++) {
+            System.out.println(data1.get(i));
+        }
+    }
+
+    public static void listSize() {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> data2 = new ArrayList<>();
+
+        while (true) {
+            String x = reader.nextLine();
+
+            if (x.equals("")) {
+                break;
+            }
+
+            data2.add(x);
+        }
+
+        System.out.println("In total: " + data2.size());
+        reader.close();
+    }
+
+    public static void onTheList() {
+        Scanner reader = new Scanner(System.in);
+        ArrayList<String> data3 = new ArrayList<>();
+
+        while (true) {
+            String x = reader.nextLine();
+
+            if (x.equals("")) {
+                break;
+            }
+
+            data3.add(x);
+        }
+
+        System.out.println("Search for?");
+        String search = reader.nextLine();
+        reader.close();
+
+        int found = 0;
+        for (int i = 0; i < data3.size(); i++) {
+            if (data3.get(i).equals(search)) {
+                System.out.println(search + " was found!");
+                found = 1;
+                break;
+            } 
+        }
+
+        if (found == 0) {
+            System.out.println(search + " was not found!");
+        }
+        
+    }
+    public static void removeLast(ArrayList<String> strings) {
+        strings.remove(strings.size() - 1);
+    }
+}
+
+
